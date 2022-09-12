9e27e0e7117ddbac0f369df23bd65ca4dc8c1184
jlucasIW <jack.lucas@infinityworks.com>
2022-09-12 15:18:55 +0100

    Update project to new AWS account

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@ export ENV="dev"
 
 echo "Create s3 bucket for state resources"
 cd aws/state_resources/s3
+rm -rf .terraform
 terraform init
 terraform workspace new $ENV || terraform workspace select $ENV
 terraform plan
@@ -9,6 +10,7 @@ terraform apply -input=false -auto-approve
 
 echo "dynamo"
 cd ../dynamoDB
+rm -rf .terraform
 terraform init
 terraform workspace new $ENV || terraform workspace select $ENV
 terraform plan
@@ -24,12 +26,13 @@ terraform apply -input=false -auto-approve
 terraform output -json
 
 echo "Generating acccess key for cicd"
-# aws iam create-access-key --user-name glasgow-cannon-1-ci-user-$ENV
+# aws iam create-access-key --user-name glasgow-cannon-2-ci-user-$ENV
 terraform state pull | jq '.resources[] | select(.type == "aws_iam_access_key") | .instances[0].attributes'
 echo "Copy the keys to github secrets"
 
 echo "Create s3 bucket for pipe to use"
 cd ../../s3
+rm -rf .terraform
 terraform init
 terraform workspace new $ENV || terraform workspace select $ENV
 terraform plan
