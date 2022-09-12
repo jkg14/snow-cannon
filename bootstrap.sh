fe62c9dae289075bf362ea6bb44f364222a032d5
Jakub Gajdul <jakub.gajdul@infinityworks.com>
2022-09-12 12:31:06 +0100

    Add bootstrap script, snowflake table definition

---
+++ bootstrap.sh
@@ -0,0 +1,36 @@
+export ENV="dev"
+
+echo "Create s3 bucket for state resources"
+cd aws/state_resources/s3
+terraform init
+terraform workspace new $ENV || terraform workspace select $ENV
+terraform plan
+terraform apply -input=false -auto-approve
+
+echo "dynamo"
+cd ../dynamoDB
+terraform init
+terraform workspace new $ENV || terraform workspace select $ENV
+terraform plan
+terraform apply -input=false -auto-approve
+
+echo "cicd user"
+cd ../../iam/ci-deployment-user
+rm -rf .terraform
+terraform init
+terraform workspace new $ENV || terraform workspace select $ENV
+terraform plan
+terraform apply -input=false -auto-approve
+terraform output -json
+
+echo "Generating acccess key for cicd"
+# aws iam create-access-key --user-name glasgow-cannon-1-ci-user-$ENV
+terraform state pull | jq '.resources[] | select(.type == "aws_iam_access_key") | .instances[0].attributes'
+echo "Copy the keys to github secrets"
+
+echo "Create s3 bucket for pipe to use"
+cd ../../s3
+terraform init
+terraform workspace new $ENV || terraform workspace select $ENV
+terraform plan
+terraform apply -input=false -auto-approve
